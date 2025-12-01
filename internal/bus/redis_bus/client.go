16af09bfc971a16c48e6ba40b683fcb821cc09e6
radostev <pavel.radostev@zyfra.com>
2025-12-01 20:20:49 +0700

    Add redis Bus

---
+++ internal/bus/redis_bus/client.go
@@ -0,0 +1,13 @@
+package redis_bus
+
+// import (
+// 	"github.com/redis/go-redis/v9"
+// )
+
+// func NewClient(addr, password string, db int) *redis.Client {
+// 	return redis.NewClient(&redis.Options{
+// 		Addr:     addr,
+// 		Password: password,
+// 		DB:       db,
+// 	})
+// }
---
+++ internal/bus/redis_bus/redis_bus.go
@@ -0,0 +1,3 @@
+package redis_bus
+
+// import (
